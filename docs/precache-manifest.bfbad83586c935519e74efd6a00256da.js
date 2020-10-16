e28092d2df4f647e1846a026c93a42775ff94fc9
Joseph Garrone <joseph.garrone.gj@gmail.com>
2020-10-16 20:42:04 +0200

    initial commit

---
+++ docs/precache-manifest.bfbad83586c935519e74efd6a00256da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1734491e4cbd168d1abc787370591606",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9b38f16f0aa2f7e31ce",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "516d5f8381ae984b2971",
+    "url": "/static/js/2.e66e4313.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.e66e4313.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9b38f16f0aa2f7e31ce",
+    "url": "/static/js/main.b030b54c.chunk.js"
+  },
+  {
+    "revision": "1b9637181d92369e8f11",
+    "url": "/static/js/runtime-main.08c8a0b4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);

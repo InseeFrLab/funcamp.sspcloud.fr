6cc65c96296019da01a10b10c9ed72575a6742b1
Joseph Garrone <joseph.garrone.gj@gmail.com>
2020-10-18 00:20:09 +0200

    Transition, half way there

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useRef, useCallback } from "react";
 import "./App.scss";
 
-export const App: React.FC = () => (
-  <div className="App">
+export const App: React.FC = () => {
 
-    <div className="castle_side">
+  const appRef = useRef<HTMLDivElement>(null);
+
+  const clickCallback = useCallback(() =>
+    appRef.current!.classList.add("fullScreen"),
+    []
+  );
+
+  return (
+    <div className="App" ref={appRef}>
+
+      <div className="castleSide">
+
+      </div>
+
+      <div className="catchphraseSide">
+
+        <h1>Le Funcamp R, de la nostalgie, du fun et du R!</h1>
+        <h2>...saurez-vous percer les mystères du royaume de Statis?</h2>
+        <button onClick={clickCallback}> Let's go </button>
+
+
+      </div>
 
     </div>
-
-  </div>
-);
+  );
+}
 

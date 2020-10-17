428d9bc4c237f733295411ce5a380a8f2a0e4567
Joseph Garrone <joseph.garrone.gj@gmail.com>
2020-10-17 16:03:15 +0200

    initial setup

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,24 +1,13 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import "animate.css";
+import "./App.scss";
 
 export const App: React.FC = () => (
   <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p className="animate__animated animate__bounce">
-        Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-        </a>
-    </header>
+
+    <div className="castle_side">
+
+    </div>
+
   </div>
 );
 

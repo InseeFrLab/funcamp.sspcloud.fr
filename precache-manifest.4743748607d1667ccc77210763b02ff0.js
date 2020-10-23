97a7f3c7829fe0502aeb487868896f23446a5716
Joseph Garrone <joseph.garrone.gj@gmail.com>
2020-10-23 04:02:53 +0200

    Updates

---
+++ precache-manifest.4743748607d1667ccc77210763b02ff0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a717ea8d3c5f120a56d1107aebc373c4",
+    "url": "/,https://garronej.github.io/funcamp/index.html"
+  },
+  {
+    "revision": "2a46803b7086856ff3fb",
+    "url": "/,https://garronej.github.io/funcamp/static/css/main.de547e24.chunk.css"
+  },
+  {
+    "revision": "ec002b478dc67a801cb7",
+    "url": "/,https://garronej.github.io/funcamp/static/js/2.ed197ddf.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/,https://garronej.github.io/funcamp/static/js/2.ed197ddf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a46803b7086856ff3fb",
+    "url": "/,https://garronej.github.io/funcamp/static/js/main.8abe9a05.chunk.js"
+  },
+  {
+    "revision": "6b9e3d6756c1541512cf",
+    "url": "/,https://garronej.github.io/funcamp/static/js/runtime-main.8784333d.js"
+  },
+  {
+    "revision": "dadc45caed9cc92fb81f2b4a4a35d16c",
+    "url": "/,https://garronej.github.io/funcamp/static/media/castle_background.dadc45ca.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8df20a46636d17d4daed8d162190abea.js"
+  "/,https://garronej.github.io/funcamp/precache-manifest.4743748607d1667ccc77210763b02ff0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/,https://garronej.github.io/funcamp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });

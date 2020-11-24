6ca79122fc4d2eb8435a99c81814925a182ec28a
Joseph Garrone <joseph.garrone.gj@gmail.com>
2020-11-24 04:21:31 +0100

    Change time of orleant event, start at 10 instead of 9

--- src/components/CountDownAndHomeCastle/CountDownAndHomeCastle.tsx
+++ src/components/CountDownAndHomeCastle/CountDownAndHomeCastle.tsx
@@ -24,7 +24,7 @@ const upcomingEvents: { [eventName: string]: { countdownTargetDate: CountdownTar
             "endtimeYear": 2020,
             "endtimeMonth": 11,
             "endtimeDate": 24,
-            "endtimeHours": 9
+            "endtimeHours": 10
         },
         "eventPageUrl": "https://docs.funcamp.sspcloud.fr/evenements/orlean_24_11_20"
     },

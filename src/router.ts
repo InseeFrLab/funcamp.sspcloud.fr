ed820d59c43dd14fbdaa9f938bed1bd3f290ab0d
Joseph Garrone <joseph.garrone.gj@gmail.com>
2020-11-29 17:47:51 +0100

    use plugin to improve SEO

--- src/router.ts
+++ src/router.ts
@@ -1,11 +1,16 @@
 import { createRouter, defineRoute } from "type-route";
+import { makeThisModuleAnExecutableRouteLister } from "github-pages-plugin-for-type-route";
 
 const publicUrl = process.env["PUBLIC_URL"]
 
-const root =  publicUrl  ? defineRoute(publicUrl) : { "extend": defineRoute };
+const root = publicUrl ? defineRoute(publicUrl) : { "extend": defineRoute };
 
-export const { RouteProvider, useRoute, routes } = createRouter({
+const routeDefs = {
   "countdown": root.extend("/"),
   "home": root.extend("/home")
-});
+};
+
+makeThisModuleAnExecutableRouteLister(routeDefs);
+
+export const { RouteProvider, useRoute, routes } = createRouter(routeDefs);
 
